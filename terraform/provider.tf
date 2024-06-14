b885c735723242395231e5b260b51539ea250347
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-06-14 13:57:33 -0400

    updated aws provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
         source  = "hashicorp/aws"
-        version = "4.17.1"
+        #version = "4.17.1"
+        version = "5.16.0"
     }
   }
 }
