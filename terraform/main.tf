d7640a8d567cfbbc4dd2bcad4e1b51319943cc2e
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-06-14 09:58:55 -0400

    updated dns version

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,7 @@ module "opensearch" {
 
 module "dns" {
   count = var.create_dns_record ? 1: 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/route53?ref=neo4j"
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/route53?ref=v1.18"
   env = terraform.workspace
   alb_zone_id = module.alb.alb_zone_id
   alb_dns_name = module.alb.alb_dns_name
