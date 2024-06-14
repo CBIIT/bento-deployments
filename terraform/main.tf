6b5c49b914bdd2ec15be1fff2437d4b746db9d55
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-06-14 12:13:20 -0400

    updated cloudformation version

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,7 +151,8 @@ module "aurora" {
 #cloudfront
 module "cloudfront" {
   count = var.create_cloudfront ? 1 : 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront?ref=cds-stage"
+  #source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront?ref=cds-stage"
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront?ref=v1.18"
   alarms = var.alarms
   domain_name = var.domain_name
   cloudfront_distribution_bucket_name = var.cloudfront_distribution_bucket_name
