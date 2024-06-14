e8cd3217b75c2921b7da166bd44507139bb78d45
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-06-14 10:41:53 -0400

    updated aurora vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,6 +145,7 @@ module "aurora" {
   allowed_ip_blocks = var.allowed_ip_blocks
   db_subnet_ids = var.db_subnet_ids
   database_name = var.database_name
+  resource_prefix = "${var.stack_name}-${terraform.workspace}"
 }
 
 #cloudfront
