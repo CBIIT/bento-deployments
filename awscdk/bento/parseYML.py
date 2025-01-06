012dae40bab3c1b34ad4809b9f343ccf3d9af474
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2025-01-06 11:26:52 -0500

    updated vars

--- awscdk/bento/parseYML.py
+++ awscdk/bento/parseYML.py
@@ -4,7 +4,7 @@ import os
 parsed_file = yaml.safe_load(open('deployments.yaml'))
 
 for svc in parsed_file['services']:
-    envVarName = svc.upper() + '_IMAGE'
+    envVarName = svc.upper() + '_IMAGE_VALUE'
     print("{}={}".format(envVarName, parsed_file['services'][svc]['image']))
     
     # os.environ[envVarName] = parsed_file['services'][svc]['image']
